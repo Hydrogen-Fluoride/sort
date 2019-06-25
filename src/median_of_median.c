f8587c322beab7c6365aa693df9949a229d6311f
Hydrogen-Fluoride <fumi226045@gmail.com>
2019-06-25 18:32:45 +0900

    中央値

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -6,42 +6,62 @@
 int A[N];
 
 // *p と *q の値を入れ替える関数
-void swap(int *p, int *q){
+void swap(int *p, int *q) {
   int tmp;
   tmp = *p;
   *p = *q;
   *q = tmp;
 }
 
-int quick_select(int A[], int n, int k){
-  int i, j, pivot;
-
-// 真ん中の要素をピボットとする
-  pivot = A[n/2];
-  A[n/2] = A[0];
-  A[0] = pivot;
-  for(i = j = 1; i < n; i++){
-    if(A[i] <= pivot){
-      swap(A+i, A+j);
-      j++;
+int bubble(int A[], int n, int k) {
+  for (int i = 0; i < n; i++) {
+    for (int j = i; j < n; j++) {
+      if (A[i] > A[j]) {
+        swap(A + i, A + j);
+      }
     }
   }
-
-  if(j == k+1) return pivot;
-  else if(j < k+1) return quick_select(A+j, n-j, k-j);
-  else return quick_select(A+1, j-1, k);
+  return A[k];
 }
 
+int quick_select(int A[], int n, int k) {
+  int i, j, pivot;
 
-int main(){
+  if (n <= 5) {
+    return bubble(A, n, k);
+  } else {
+    int Aa[N];
+    for (int i = 0; i < n / 5; i++) {
+      Aa[i] = bubble(A + i * 5, 5, 2);
+    }
+    pivot = quick_select(Aa, n / 5, n / 10);
+    j = 0;
+    for (i = 0; i < n; i++) {
+      if (A[i] <= pivot) {
+        swap(A + i, A + j);
+        j++;
+      }
+    }
+    if (j == k + 1) {
+      return pivot;
+    } else if (j < k + 1) {
+      return quick_select(A + j, n - j, k - j);
+    } else {
+      return quick_select(A, j, k);
+    }
+  }
+}
+
+int main() {
   int i;
   A[0] = 0;
   A[1] = 17; //原始元
-  for(i=2;i<N;i++){
-    A[i] = (long long int) A[i-1] * A[1] % N;
+  for (i = 2; i < N; i++) {
+    A[i] = (long long int)A[i - 1] * A[1] % N;
   }
-  for(i=0;i<N;i++){
-    if(quick_select(A, N, i) != i) printf("ERROR %d %d\n", i, quick_select(A, N, i));
-//    printf("%d th element is %d\n", i, quick_select(A, N, i));
+  for (i = 0; i < N; i++) {
+    if (quick_select(A, N, i) != i)
+      printf("ERROR %d %d\n", i, quick_select(A, N, i));
+    //    printf("%d th element is %d\n", i, quick_select(A, N, i));
   }
 }
