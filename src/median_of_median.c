d38b8dee36e3b30fd73539a916453023f5a712c4
Hydrogen-Fluoride <fumi226045@gmail.com>
2019-07-09 08:47:59 +0900

    5の倍数修正

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -30,13 +30,16 @@ int quick_select(int A[], int n, int k) {
   if (n <= 5) {
     return bubble(A, n, k);
   } else {
-    int Aa[N / 5 + 1];
+    int Aa[(N - 4) / 5];
     for (i = 0; i < n / 5; i++) {
       Aa[i] = bubble(A + i * 5, 5, 2);
     }
-    Aa[n / 5] = bubble(A + n / 5, n % 5, (n % 5) / 2);
-    
-    pivot = quick_select(Aa, n / 5 + 1, n / 10);
+    if (n % 5 != 0) {
+      Aa[n / 5] = bubble(A + n - n % 5, n % 5, (n % 5) / 2);
+      pivot = quick_select(Aa, n / 5 + 1, (n / 5 + 1) / 2);
+    } else {
+      pivot = quick_select(Aa, n / 5, n / 10);
+    }
     for (i = 0; i < n; i++) {
       if (A[i] == pivot) {
         break;
